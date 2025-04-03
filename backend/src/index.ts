8f988516732edac469fb638c193268e51793f51c
Warren Hamdi <warren.hamdi@gmail.com>
2025-04-03 13:40:42 +0200

    [TRANSCENDANCE][backend][POST] -> refactoring post

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,8 +5,8 @@ import cors from '@fastify/cors';
 import db from '../db';
 import bcrypt from 'fastify-bcrypt';
 const { serialize, parse } = require('@fastify/cookie')
-
-const server = fastify();
+import { postRoute } from "./route/post";
+export const server = fastify();
 
 server.register(bcrypt, { saltWorkFactor: 12 });
 //FIX: Import jwt dynamically from .env file
@@ -24,63 +24,14 @@ server.register(cors, {
   credentials: true,
 });
 
-interface User {
+export interface User {
   id?: number;
   name: string;
   surname: string;
   email: string;
   password: string;
 }
-
-// Route pour ajouter un utilisateur
-server.post('/users', async (request: FastifyRequest<{ Body: User }>, reply: FastifyReply) => {
-  const { name, surname, email, password } = request.body;
-
-  if (!name || !surname || !email || !password) {
-    return reply.status(400).send({ error: "Tous les champs sont obligatoires" });
-  }
-
-  try {
-    // Vérifier si l'utilisateur existe déjà
-    const userExists = await new Promise<boolean>((resolve, reject) => {
-      db.get('SELECT email FROM users WHERE email = ?', [email], (err, row) => {
-        if (err) return reject(err);
-        resolve(!!row);
-      });
-    });
-
-    if (userExists) {
-      return reply.status(409).send({ error: "Cet email est déjà utilisé" });
-    }
-
-    const hashedPass = await server.bcrypt.hash(password);
-
-    const userId = await new Promise<number>((resolve, reject) => {
-      db.run(
-        'INSERT INTO users (name, surname, email, password) VALUES (?, ?, ?, ?)',
-        [name, surname, email, hashedPass],
-        function (err: Error | null) {
-          if (err) return reject(new Error("Erreur lors de l'insertion de l'utilisateur"));
-          resolve(this.lastID); // Récupérer l'ID de l'utilisateur ajouté
-        }
-      );
-    });
-
-    const token = server.jwt.sign({ id: userId, email}, {expiresIn: 3600 }); 
-    reply.setCookie('access_token', token, {
-      path: '/',
-      httpOnly: true,
-      secure: true,
-	  maxAge: 3600
-    });
-
-    return reply.status(201).send({ accessToken: token, message: "Utilisateur créé avec succès" });
-  } catch (e: any) {
-    console.error(e);
-    return reply.status(500).send({ error: "Erreur serveur" });
-  }
-});
-
+ postRoute(server); // check tout le shmilbique pour export cette merde 
 // Route pour récupérer tous les utilisateurs (sans mot de passe)
 server.get('/users', (_request: FastifyRequest, reply: FastifyReply) => {
   db.all('SELECT id, name, surname, email FROM users', [], (err, rows: Omit<User, 'password'>[]) => {
---
+++ backend/src/route/post.ts
@@ -0,0 +1,61 @@
+import {server} from '../index'
+import fastify, { FastifyInstance, FastifyReply, FastifyRequest } from 'fastify';
+import fjwt from '@fastify/jwt';
+import fCookie from '@fastify/cookie';
+import cors from '@fastify/cors';
+import db from '../../db';
+import bcrypt from 'fastify-bcrypt';
+import {User} from '../index'
+// Route pour ajouter un utilisateur
+
+export async function postRoute(server : FastifyInstance)
+{
+	server.post('/users', async (request: FastifyRequest<{ Body: User }>, reply: FastifyReply) => {
+		const { name, surname, email, password } = request.body;
+
+		if (!name || !surname || !email || !password) {
+			return reply.status(400).send({ error: "Tous les champs sont obligatoires" });
+		}
+
+		try {
+			// Vérifier si l'utilisateur existe déjà
+			const userExists = await new Promise<boolean>((resolve, reject) => {
+				db.get('SELECT email FROM users WHERE email = ?', [email], (err, row) => {
+					if (err) return reject(err);
+					resolve(!!row);
+				});
+			});
+
+			if (userExists) {
+				return reply.status(409).send({ error: "Cet email est déjà utilisé" });
+			}
+
+			const hashedPass = await server.bcrypt.hash(password);
+
+			const userId = await new Promise<number>((resolve, reject) => {
+				db.run(
+					'INSERT INTO users (name, surname, email, password) VALUES (?, ?, ?, ?)',
+					[name, surname, email, hashedPass],
+					function (err: Error | null) {
+						if (err) return reject(new Error("Erreur lors de l'insertion de l'utilisateur"));
+						resolve(this.lastID); // Récupérer l'ID de l'utilisateur ajouté
+					}
+				);
+			});
+
+			const token = server.jwt.sign({ id: userId, email}, {expiresIn: 3600 }); 
+			reply.setCookie('access_token', token, {
+				path: '/',
+				httpOnly: true,
+				secure: true,
+				maxAge: 3600
+			});
+
+			return reply.status(201).send({ accessToken: token, message: "Utilisateur créé avec succès" });
+		} catch (e: any) {
+			console.error(e);
+			return reply.status(500).send({ error: "Erreur serveur" });
+		}
+	});
+}
+
