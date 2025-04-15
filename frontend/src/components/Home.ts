0094a0aa3bc109e9a2b434e1deec59c9c26d876c
ninoCLS <ninoclasseau01@gmail.com>
2025-04-15 16:32:05 +0200

    front - add ssl / docker / connect back & front

--- frontend/src/components/Home.ts
+++ frontend/src/components/Home.ts
@@ -1,10 +1,45 @@
 import { PongNode } from "../lib/PongNode";
-import { Div, P } from "../lib/PongFactory"
+import { Div, P, Button } from "../lib/PongFactory"
 
 
 export function Home(): PongNode<any> {
 
+	const body = {
+		// name: "test2",
+		// surname: "test2eeee",
+		email: "test2@gmail.com",
+		password: "jspppffff"
+	};
+
+	const sendBody = () => {
+		fetch("http://localhost:3000/login", {
+			method: "POST",
+			body: JSON.stringify(body),
+			headers: {
+				'Content-Type': 'application/json',
+			}
+		})
+		.then(res => {
+			if (!res.ok)
+				throw new Error(`HTTP error! Status: ${res.status}`);
+			return res.text();
+		})
+		.then(body => {
+			console.log("res brute :", body);
+			try {
+			  const parsedBody = JSON.parse(body);
+			  console.log("Body parsed:", parsedBody);
+			} catch (e) {
+			  console.error("Erreur de parsing JSON :", e);
+			}
+		})
+		.catch(e => console.error("Erreur :", e));
+	}
+
+
 	return Div({}, [
 		P({}, ["Welcome to Home page !"]),
+		Button({id: "b2", onClick: sendBody }, ["Login test"]),
+		
 	]);
 }
