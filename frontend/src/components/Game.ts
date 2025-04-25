eda0e31efc1052a6d67bc38a1b084072704229d2
Oscar Chetrit <ochetrit@made-f0Br8s13.clusters.42paris.fr>
2025-04-25 15:41:24 +0200

    push jsp

--- frontend/src/components/Game.ts
+++ frontend/src/components/Game.ts
@@ -1,22 +1,81 @@
 import { log } from "console";
-import { Div, Button, P } from "../lib/PongFactory";
+import { Div, Button, P, Span, Li } from "../lib/PongFactory";
 import { PongNode } from "../lib/PongNode";
 import { rerender } from "../router/router"
 
 var playerCount = 1;
 
 export function Game(): PongNode<any> {
-	return Div({ class: "flex flex-col justify-between items-center min-h-screen p-15" }, [Div({class: "grid grid-rows-[40%_15%_5%_40%] min-h-screen"}, [
-		P({ class: "text-2xl font-bold text-gray-800" }, ["Welcome to Pong"]),
-		
-		PlayerSelector(),
-		Button({
-			id: "startgame",
-			class: "bg-green-500 hover:bg-green-400 text-white font-bold py-2 px-6 border-b-4 border-green-700 hover:border-green-500 rounded"
-		}, ["Start Game"]),
-	]),
+	setTimeout(() => startPongBall(), 0);
+
+	return Div({ class: "flex flex-col justify-around items-center min-h-screen p-5 bg-black" }, [
+			Span({ class: "block font-orbitron md:text-5xl text-yellow-400" }, ["Pong like you’ve never played it before."]),
+			Span({ class: "block font-orbitron md:text-3xl text-yellow-400" }, ["Choose your map:"]),
+			
+			//PlayerSelector(),
+			Button({
+				id: "Select-map",
+				class: "bg-yellow-500 hover:bg-yellow-400 text-white font-bold py-2 px-4 border-b-4 border-yellow-700 hover:border-yellow-500 rounded"
+			}, ["Select map"]),
+			Div({ id: "game-area", class: "relative w-[600px] h-[400px] bg-zinc-900 overflow-hidden" }, [
+				Div({ id: "ball", class: "absolute w-[20px] h-[20px] bg-yellow-400 rounded-full" }),
+				Div({id: "leftpad", class:"absolute w-[15px] h-[80px] bg-yellow-400 rounded-full left-[5px] top-[160px]"}),
+				Div({id: "rightpad", class:"absolute w-[15px] h-[80px] bg-yellow-400 rounded-full left-[580px] top-[160px]"})
+			]),
+			Div({ class: "flex flex-col justify-around items-center h-30"}, [
+				Span({ class: "block font-orbitron md:text-2xl text-yellow-400" }, ["Go solo or battle your friends!:"]),
+				Div({ class: "flex justify-between w-90"}, [
+					Button({id: "sgplayerButton",
+						class: "bg-yellow-500 hover:bg-yellow-400 text-white font-bold py-2 px-4 border-b-4 border-yellow-700 hover:border-yellow-500 rounded"},
+						["Single Player"]),
+					Button({id: "mgplayerButton",
+						class: "bg-yellow-500 hover:bg-yellow-400 text-white font-bold py-2 px-4 border-b-4 border-yellow-700 hover:border-yellow-500 rounded"},
+						["Multiple Player"]),
+				]),
+			]),
 	]);
 }
+
+function startPongBall(){
+	const ball = document.getElementById("ball") as HTMLDivElement;
+	const gameArea = document.getElementById("game-area") as HTMLDivElement;
+
+	if (!ball || !gameArea)
+		return ;
+
+	let x = 100;
+	let y = 100;
+	let dx = 8;
+	let dy = 8;
+
+	const areaWidth = gameArea.clientWidth;
+	const areaHeight = gameArea.clientHeight;
+	const ballSize = ball.clientWidth;
+
+	function moveBall(){
+		x += dx;
+		y += dy;
+		if (x <= 0 || x + ballSize >= areaWidth)
+			dx *= -1;
+		if (y <= 0 || y + ballSize >= areaHeight)
+			dy *= -1;
+		if ((y <= 160 && y >= 240) && (x <= 20))
+			dx *= -1;
+		if ((y >= 160 && y <= 240) && (x + ballSize >= areaWidth - 20))
+			dx *= -1;
+		if (y + ballSize >= 160 && y + ballSize - dy <= 160 && (x + ballSize >= areaWidth - 20 || x <= 20))
+			dy *= -1;
+		if (y + ballSize >= 160 && y + ballSize - dy <= 160 && (x + ballSize >= areaWidth - 20 || x <= 20))
+			dy *= -1;
+		ball.style.left = `${x}px`;
+		ball.style.top = `${y}px`;
+
+		requestAnimationFrame(moveBall);
+	}
+
+	requestAnimationFrame(moveBall);
+}
+
 
 export function PlayerSelector(): PongNode<any> {
 
