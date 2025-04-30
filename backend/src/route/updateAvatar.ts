3de81c86b87fade0e9554496ce7d2ba7c0a12561
Warren Hamdi <warren.hamdi@gmail.com>
2025-04-30 14:34:46 +0200

    [Backend] -> Working on User management (Update Avatar done)

--- backend/src/route/updateAvatar.ts
+++ backend/src/route/updateAvatar.ts
@@ -1,21 +1,30 @@
 
 import { FastifyReply, FastifyRequest } from "fastify";
 import db from "../../db";
-
+import fs from 'fs'
+import path from 'path';
 
 interface UpdateMailBody {
 	userId: number;
 	avatarpath: string;
 }
 
-export const updateMail = async (request: FastifyRequest<{ Body: UpdateMailBody }>, reply: FastifyReply) => {
+export const updateAvatar = async (request: FastifyRequest<{ Body: UpdateMailBody }>, reply: FastifyReply) => {
 	const { userId, avatarpath } = request.body;
-	const files = await request.savedRequestFiles();
-	files[0].filepath = "../../public";
+	const files = await request.file();
+	if (!files || !userId) {
+		return reply.status(400).send({ error: "Missing file or userId" });
+  }
 
+	const filename = `avatar-user-${userId}-${Date.now()}-${files.filename}`;
+	const uploadPath = path.join(__dirname, '../../public/uploads', filename);
+
+	const stream = fs.createWriteStream(uploadPath);
+	await files.file.pipe(stream);
+	const relativePath = `/uploads/${filename}`;
 	try {
 		const result = await new Promise((resolve, reject) => {
-			db.run('UPDATE users SET avatar_path = ? WHERE id = ?', [avatarpath, userId], function (err) {
+			db.run('UPDATE users SET avatar_path = ? WHERE id = ?', [relativePath, userId], function (err) {
 				if (err) return reject(err);
 				if (this.changes === 0) return reject(new Error("User not found"));
 				resolve("Avatar updated");
