0090414bbd45f9349a60e06fcfc22d9162476788
Warren Hamdi <warren.hamdi@gmail.com>
2025-05-02 13:26:31 +0200

    [Backend]-> created getGameScore to get the history of matches

---
+++ backend/src/route/getGameScore.ts
@@ -0,0 +1,43 @@
+import { FastifyRequest, FastifyReply } from 'fastify';
+import db from '../../db';
+
+type JWTClaims = {
+	id: string,
+	email: string,
+};
+
+const dbGet = (sql: string, params: any[]) => {
+	return new Promise<any>((resolve, reject) => {
+		db.get(sql, params, (err, row) => {
+			if (err) return reject(err);
+			resolve(row);
+		});
+	});
+};
+
+export const me = async (request: FastifyRequest, reply: FastifyReply) => {
+	try {
+		const token = request.cookies["access_token"];
+		if (!token) {
+			return reply.status(401).send({ error: 'Token manquant' });
+		}
+
+		const claims = request.server.jwt.decode<JWTClaims>(token);
+		const userId = claims!.id;
+
+		const row = await dbGet(
+			'SELECT id, user_id , result , guest_name , game_date FROM game WHERE id = ?',
+			[userId]
+		);
+
+		if (!row) {
+			return reply.status(404).send({ error: 'Utilisateur non trouvé' });
+		}
+
+		return reply.status(200).send(row);
+
+	} catch (err) {
+		console.error(err);
+		return reply.status(401).send({ error: 'JWT invalide, expiré ou erreur serveur' });
+	}
+};
