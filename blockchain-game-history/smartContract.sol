004622776299f3f7b9c2ff894415e4ed021caffb
Warren Hamdi <warren.hamdi@gmail.com>
2025-05-12 19:23:18 +0200

    [Blockchain] -> started the tournament SmartContract(added event, struct,get and post)

--- blockchain-game-history/smartContract.sol
+++ blockchain-game-history/smartContract.sol
@@ -1,23 +1,27 @@
-
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.26;
+pragma solidity ^0.8.0;
 
-contract Counter {
-    uint256 public count;
+contract ScorePong {
 
-    // Function to get the current count
-    function get() public view returns (uint256) {
-        return count;
+    struct GameScore {
+        uint256 id;
+        string date;
+        bool won;
+        string guest_name;
     }
 
-    // Function to increment count by 1
-    function inc() public {
-        count += 1;
+    mapping(uint256 => GameScore) public scores;
+    uint256 public scoreCount;
+
+    event GetTournamentScores(uint id, string date, bool won, string guest_name);
+
+    function putScore(string memory _date, bool _won, string memory _guest_name) public {
+        scoreCount++;
+        scores[scoreCount] = GameScore(scoreCount, _date, _won, _guest_name);
     }
 
-    // Function to decrement count by 1
-    function dec() public {
-        // This function will fail if count = 0
-        count -= 1;
+    function getScore(uint256 _id) public view returns (GameScore memory) {
+        require(_id > 0 && _id <= scoreCount, "Score ID invalide");
+        return scores[_id];
     }
 }
