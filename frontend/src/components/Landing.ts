d3f66d100b16cd83023de58266b5ffa9cb298302
ninoCLS <ninoclasseau01@gmail.com>
2025-05-22 14:55:49 +0200

    ADD 42LOGO

--- frontend/src/components/Landing.ts
+++ frontend/src/components/Landing.ts
@@ -24,7 +24,7 @@ function navLanding(): PongNode<any> {
 		Div({
 			class: "fixed top-0 left-0 right-0 z-50 flex items-center justify-between p-4 bg-black shadow-md"
 		}, [
-			Image({ id: "img", src: "/assets/42logoT.png", class: "h-10" }),
+			Image({ id: "img", src: "/assets/42logo.png", class: "h-10" }),
 			Div({ class: "flex gap-4"}, [
 				// Boutons de langue FR / EN / PT
 				Button({
