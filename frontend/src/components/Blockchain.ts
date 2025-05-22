f9d9e4f26c272f1810acdeb4e5ea3e1f3d252794
Cyprien Ferreira <cyferrei@f1br8s3.clusters.42paris.fr>
2025-05-22 14:31:24 +0200

    [ADD]: translation for Blockchain.ts OK

--- frontend/src/components/Blockchain.ts
+++ frontend/src/components/Blockchain.ts
@@ -2,6 +2,10 @@ import { Div, UList, Li, P } from "../lib/PongFactory";
 import { PongNode } from "../lib/PongNode";
 import { areaCss } from "../styles/cssFactory";
 import {rerender} from "../router/router"
+
+import { t } from "i18next";
+import i18n from "i18next";
+
 type Score = {
 	id: number;
 	date: string;
@@ -35,13 +39,13 @@ export  function Blockchain(): PongNode<any> {
 	fetchBlockchainData();
 
 	return Div({ class: `${areaCss} min-h-screen bg-yellow-400 flex flex-col items-center pt-20` }, [
-		P({ class: "text-3xl font-bold mb-6" }, ["Historique des parties Blockchain"]),
+		P({ class: "text-3xl font-bold mb-6" }, [t("blockchain.game_history")]),
 
 		Div({ class: "w-full max-w-4xl bg-yellow-300 rounded-lg p-6 shadow-md" }, [
 			UList({ class: "grid grid-cols-3 font-semibold border-b border-gray-700 pb-2 mb-2 text-center" }, [
-				Li({}, ["Date"]),
-				Li({}, ["Adversaire"]),
-				Li({}, ["Resultat"]),
+				Li({}, [t("blockchain.date")]),
+				Li({}, [t("blockchain.opponent")]),
+				Li({}, [t("blockchain.result")]),
 			]),
 
 			...scores.map((score) =>
